de679f9321178131c70829ee67346c6f34ef41df
ushzkakoj <146225027+ushzkakoj@users.noreply.github.com>
2025-06-30 13:31:14 +0200

    update

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ const divide = (a, b) => {
 console.log(calculate(10, 5, add));      
 console.log(calculate(10, 5, subtract)); 
 console.log(calculate(10, 5, multiply)); 
+console.log(calculate(10, 5, divide));   
 console.log(calculate(10, 0, divide));   
 
 function repeatMessage(times, messageCreator) {
